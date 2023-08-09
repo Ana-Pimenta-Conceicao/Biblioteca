236361c611c49c331f34843f057441e87cfa4955
Ana-Pimenta-Conceicao <ana.pimenta120505@gmail.com>
2023-08-09 16:06:16 -0300

    Arrumado

--- Biblioteca/Data/ApplicationDBContext.cs
+++ Biblioteca/Data/ApplicationDBContext.cs
@@ -1,6 +1,13 @@
-﻿namespace Biblioteca.Data
+﻿using Biblioteca.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Biblioteca.Data
 {
-    public class ApplicationDBContext
+    public class ApplicationDBContext : DbContext
     {
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { 
+            
+        }
+        public DbSet<EmprestimoModel> Emprestimo { get; set; }  
     }
 }
--- Biblioteca/Models/EmprestimoModel.cs
+++ Biblioteca/Models/EmprestimoModel.cs
@@ -2,5 +2,11 @@
 {
     public class EmprestimoModel
     {
+        //modelo do banco de dados 
+        public int Id { get; set; }
+        public string Recebedor { get; set; }
+        public string Fornecedor { get; set;}
+        public string LivroEmprestado { get; set;}
+        public DateTime DataEmprestimo { get; set;} = DateTime.Now; //Data Atual
     }
 }
